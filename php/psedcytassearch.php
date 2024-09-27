996353a37bd5c15b3d4ac1bdb0e14c1ef97191ba
JTiepmar <jochen.tiepmar@serbski-institut.de>
2024-09-27 11:35:14 +0200

    ...

--- php/psedcytassearch.php
+++ php/psedcytassearch.php
@@ -4,7 +4,7 @@ header('Content-Type: text/plain');
 if (isset($_GET['token'])){
 	$token = $_GET['token'];
 	$token = str_replace(array("ł","ć","č","ė","é","ě","ź","ž","ś","š","ŕ","ó"),array("l4","c1","c2","e1","e1","e2","z1","z2","s1","s2","r1","o1"),$token);
-	$token = str_replace(',','%" OR url LIKE "',$token);
+	$token = str_replace(',','-%" OR url LIKE "',$token);
 
 	$PDO = new PDO('sqlite:../data/psedcytas.db');
 	$query = 'SELECT url FROM urls';
