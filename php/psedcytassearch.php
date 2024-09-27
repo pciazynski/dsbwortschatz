3a625ebc202812837b4cc8f766ccdc3ab73644f4
JTiepmar <jochen.tiepmar@serbski-institut.de>
2024-09-27 11:31:36 +0200

    vorlese varianten

--- php/psedcytassearch.php
+++ php/psedcytassearch.php
@@ -3,15 +3,16 @@ header('Content-Type: text/plain');
 
 if (isset($_GET['token'])){
 	$token = $_GET['token'];
-}
-if (strlen($token)>=1){
-	$nl = "\n";
+	$token = str_replace(array("ł","ć","č","ė","é","ě","ź","ž","ś","š","ŕ","ó"),array("l4","c1","c2","e1","e1","e2","z1","z2","s1","s2","r1","o1"),$token);
+	$token = str_replace(',','%" OR url LIKE "',$token);
+
 	$PDO = new PDO('sqlite:../data/psedcytas.db');
 	$query = 'SELECT url FROM urls';
-	$query .= ' WHERE url LIKE "'.$token.'%" LIMIT 1';
+	$query .= ' WHERE url LIKE "'.$token.'-%" LIMIT 1';
+	
 	$result = $PDO->query($query.";");
 	foreach($result as $row){
-		print($row['url'].$nl);
+		print($row['url']);
 	}
 }
 ?>
