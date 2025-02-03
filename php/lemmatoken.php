579df9396b361fbabc07e44746dd62005f630a61
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-02-03 15:10:43 +0100

    ...

--- php/lemmatoken.php
+++ php/lemmatoken.php
@@ -5,17 +5,18 @@ if (isset($_GET['lemma'])){
 
 	$PDO = new PDO('sqlite:../data/lemmamapping.db');
 	$query = 'SELECT token, lemma, frequency FROM lemmatokenfrequency';
-	if (isset($_GET['inclusive'])){
-		$lemma = str_replace(',','|%" OR lemma LIKE "%|',$_GET['lemma']);
-		$query .= ' WHERE lemma LIKE "%|'.$lemma.'|%"';
-	}
-	else{
+	if (isset($_GET['exact'])){
 		$lemma = str_replace(',','|" OR lemma = "|',$_GET['lemma']);
 		$query .= ' WHERE lemma = "|'.$lemma.'|"';
+	}
+	else{
+		$lemma = str_replace(',','|%" OR lemma LIKE "%|',$_GET['lemma']);
+		$query .= ' WHERE lemma LIKE "%|'.$lemma.'|%"';
 	}
 	if (isset($_GET['sort'])){
 		$query .= ' ORDER BY frequency DESC, token';
 	}
+	
 	$result = $PDO->query($query.";");
 	
 	$tab = "\t";
