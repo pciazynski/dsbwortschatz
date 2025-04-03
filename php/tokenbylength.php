798fdcb276486d2f06ce4c451912e079c9014742
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-04-03 15:56:42 +0200

    ..

--- php/tokenbylength.php
+++ php/tokenbylength.php
@@ -6,7 +6,7 @@ if(isset($_GET['length'])){
 	(isset($_GET['limit']))?$limit=$_GET['limit']:$limit=50;
 	(isset($_GET['random']))?$order='random()':$order='token';
 	(isset($_GET['frequency']))?$frequency=' AND frequency '.$_GET['frequency']:$frequency='';
-	(isset($_GET['clean']))?$clean=' AND frequency>2':$clean='';
+	(isset($_GET['clean']))?$clean=' AND frequency>9':$clean='';
 
 	#SUBSTR(token,1,1) and GROUP BY sub make sure that the words are not very similar
 	$query = 'SELECT SUBSTR(token,1,1) as sub , token FROM tokencount WHERE LENGTH(token)=='.$_GET['length'].$frequency.$clean.' GROUP BY sub ORDER BY '.$order.' DESC LIMIT '.$limit;
