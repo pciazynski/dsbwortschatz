a0a7e6a3f7d3d842805d50004e09f35d3a783832
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-05-05 16:04:58 +0200

    ..

--- php/tokenprofile.php
+++ php/tokenprofile.php
@@ -16,40 +16,40 @@ if(isset($_GET['token'])){
 		$res.=$row['mindate'].$tab.$row['maxdate'].$nl;
 	}
 	
-	$PDO1 = new PDO('sqlite:../data/lemmamapping.db');
+	$PDO = new PDO('sqlite:../data/lemmamapping.db');
 
 	$query = 'SELECT DISTINCT lemma FROM lemmatokenfrequency WHERE token = "'.$token.'"';
-	foreach($PDO1->query($query.';') as $row){
+	foreach($PDO->query($query.';') as $row){
 		$lemma=$row['lemma'];
 	}
 	$res.=$lemma.$nl;
 
 	$query = 'SELECT token FROM lemmatokenfrequency WHERE lemma LIKE "%'.$lemma.'%"';
-	foreach($PDO1->query($query.';') as $row){
+	foreach($PDO->query($query.';') as $row){
 		$res.=$row['token'].$tab;
 	}
 	$res=trim($res).$nl;
 
 	$query = 'SELECT DISTINCT norm FROM tokenlemmanormtypesubtypedatefrequency WHERE token = "'.$token.'"';
-	foreach($PDO1->query($query.';') as $row){
+	foreach($PDO->query($query.';') as $row){
 		$res.=$row['norm'].$tab;
 	}
 	$res=trim($res).$nl;
 	
 	$query = 'SELECT DISTINCT type FROM tokenlemmanormtypesubtypedatefrequency WHERE token = "'.$token.'"';
-	foreach($PDO1->query($query.';') as $row){
+	foreach($PDO->query($query.';') as $row){
 		(strlen(trim($row['type']))>0) ? $res.=$row['type'].$tab:NULL;
 	}
 	$res=trim($res).$nl;
 	
-	$PDO1 = new PDO('sqlite:../data/lemmamapping.db');
+	$PDO = new PDO('sqlite:../data/lemmamapping.db');
 	$query = 'SELECT left FROM collocation WHERE token = "'.$token.'" ORDER BY logdice DESC LIMIT 10';
-	foreach($PDO1->query($query.';') as $row){
+	foreach($PDO->query($query.';') as $row){
 		$lemma=$row['lemma'].$tab;;
 	}
 	$res.=$lemma.$nl;
 	$query = 'SELECT right FROM collocation WHERE token = "'.$token.'" ORDER BY logdice DESC LIMIT 10';
-	foreach($PDO1->query($query.';') as $row){
+	foreach($PDO->query($query.';') as $row){
 		$lemma=$row['lemma'].$tab;;
 	}
 	$res.=$lemma.$nl;
