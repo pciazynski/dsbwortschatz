4045e0a1b214cfb1ba187b7441a4e723f1d2ed8e
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-05-06 11:07:35 +0200

    ..

--- php/tokenprofile.php
+++ php/tokenprofile.php
@@ -24,35 +24,37 @@ if(isset($_GET['token'])){
 	}
 	$res.=$lemma.$nl;
 
-	$query = 'SELECT token FROM lemmatokenfrequency WHERE lemma LIKE "%'.$lemma.'%"';
-	foreach($PDO->query($query.';') as $row){
-		$res.=$row['token'].$tab;
+	if (strlen($lemma)>0){
+		$query = 'SELECT token FROM lemmatokenfrequency WHERE lemma LIKE "%'.$lemma.'%"';
+		foreach($PDO->query($query.';') as $row){
+			$res.=$row['token'].$tab;
+		}
 	}
-	$res=trim($res).$nl;
+	$res=trim($res,$tab).$nl;
 
 	$query = 'SELECT DISTINCT norm FROM tokenlemmanormtypesubtypedatefrequency WHERE token = "'.$token.'"';
 	foreach($PDO->query($query.';') as $row){
 		$res.=$row['norm'].$tab;
 	}
-	$res=trim($res).$nl;
+	$res=trim($res,$tab).$nl;
 	
 	$query = 'SELECT DISTINCT type FROM tokenlemmanormtypesubtypedatefrequency WHERE token = "'.$token.'"';
 	foreach($PDO->query($query.';') as $row){
 		(strlen(trim($row['type']))>0) ? $res.=$row['type'].$tab:NULL;
 	}
-	$res=trim($res).$nl;
+	$res=trim($res,$tab).$nl;
 	
 	$PDO = new PDO('sqlite:../data/collocation.db');
 	$query = 'SELECT left FROM collocation WHERE right = "'.$token.'" ORDER BY logdice DESC LIMIT 10';
 	foreach($PDO->query($query.';') as $row){
-		$res.=$row['left'].$tab;;
+		$res.=$row['left'].$tab;
 	}
-	$res=trim($res).$nl;
+	$res=trim($res,$tab).$nl;
 	$query = 'SELECT right FROM collocation WHERE left = "'.$token.'" ORDER BY logdice DESC LIMIT 10';
 	foreach($PDO->query($query.';') as $row){
-		$res.=$row['right'].$tab;;
+		$res.=$row['right'].$tab;
 	}
-	$res=trim($res).$nl;
+	$res=trim($res,$tab).$nl;
 	print($res);
 }
 ?>
