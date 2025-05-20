f8afff555ec13e12854a352ad28ec20be8058882
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-05-20 16:48:35 +0200

    ..

--- php/prefixlemmasearch.php
+++ php/prefixlemmasearch.php
@@ -6,14 +6,14 @@ header('Content-Type: text/plain');
 if (strlen($lemma)>=1){
 	$PDO = new PDO('sqlite:../data/lemmamapping.db');
 	(isset($_GET['limit'])) ? $limit = $_GET['limit'] : $limit = 100;
-	(isset($_GET['cutoff'])) ? $cutoff = ' GROUP BY SUBSTRING(lemma,0,'.strlen($lemma)+$_GET['cutoff'].')' : $cutoff = '';
+	(isset($_GET['cutoff'])) ? $cutoff = ' GROUP BY SUBSTRING(lemma,1,'.strlen($lemma)+$_GET['cutoff'].')' : $cutoff = '';
 	
 	$query = 'SELECT DISTINCT lemma FROM lemmafrequency WHERE lemma LIKE "|'.$lemma.'%"'.$cutoff.' ORDER BY frequency DESC LIMIT '.$limit;
 
 	$nl = "\n";
 	$res = '';
 	foreach($PDO->query($query.';') as $row){
-		$res.=$row['lemma'].$nl;
+		$res.=trim($row['lemma'],"|").$nl;
 	}
 	print($res);
 }
