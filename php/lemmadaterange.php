db602817705c589d00cbe901cff84a62904f8e73
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-05-27 11:45:48 +0200

    typeprofile -> wordprofile

---
+++ php/lemmadaterange.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: text/plain');
+
+	$PDO = new PDO('sqlite:../data/lemmamapping.db');
+	$query = 'SELECT Min(date) as mindate, Max(date) as maxdate FROM tokenlemmanormtypesubtypedatefrequency ';
+	
+	$tab = "\t";
+	$nl = "\n";
+	$res = '';
+
+	foreach($PDO->query($query.';') as $row){
+		$res.=$row['mindate'].$tab.$row['maxdate'].$nl;
+	}
+	print($res);
+?>
