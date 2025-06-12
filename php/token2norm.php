456b986c80f85a42877ed0673a19eabcc806aa27
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-06-12 10:41:59 +0200

    normvariation

---
+++ php/token2norm.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: text/plain');
+
+if (isset($_GET['token'])){
+
+	$PDO = new PDO('sqlite:../data/normmapping.db');
+	$query = 'SELECT DISTINCT lemma,token,norm,type,subtype FROM tokenlemmanormtypesubtypedatefrequency';
+	$query .= ' WHERE token = "'.$_GET['token'].'"';
+
+	$tab = "\t";
+	$nl = "\n";
+	$res = '';
+
+	foreach($PDO->query($query.';') as $row){
+		$res.=$row['lemma'].$tab.$row['token'].$tab.$row['norm'].$tab.$row['type'].$tab.$row['subtype'].$nl;
+	}
+	
+	print($res);
+}
+?>
