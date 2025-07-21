c34011f706edfa385488ce942b9073a607388bbd
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-07-21 15:56:22 +0200

    transl

--- php/normbylength.php
+++ php/normbylength.php
@@ -7,7 +7,7 @@ if(isset($_GET['length'])){
 	(isset($_GET['random']))?$order='random()':$order='norm';
 	(isset($_GET['frequency']))?$frequency=' AND frequency '.$_GET['frequency']:$frequency='';
 
-	#SUBSTR(lemma,2,2) and GROUP BY sub make sure that the words are not very similar
+	#SUBSTR(norm,2,2) and GROUP BY sub make sure that the words are not very similar
 	$query = 'SELECT SUBSTR(norm,2,2) as sub , norm FROM normfrequency WHERE LENGTH(norm)=='.($_GET['length']+2).$frequency.' GROUP BY sub ORDER BY '.$order.' DESC LIMIT '.$limit;
 
 	$nl = "\n";
