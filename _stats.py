f810ca5333721236f54eba67ec6f86dba22a388b
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-08-21 12:06:45 +0200

    ..

--- _stats.py
+++ _stats.py
@@ -4,7 +4,6 @@ import shutil
 shutil.copy("data/bagofwords/_tokensumperyear.txt","data/stats/_tokensumperyear.txt")
 shutil.copy("data/bagofwords/_typesumperyear.txt","data/stats/_typesumperyear.txt")
 shutil.copy("data/bagofwords/_typetokenratioperyear.txt","data/stats/_typetokenratioperyear.txt")
-exit()
 
 if os.path.exists("data/stats"):
     shutil.rmtree("data/stats")
