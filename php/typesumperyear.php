143e86e0da0c0e130e5bc6f5e857981de3796a98
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-08-21 09:35:03 +0200

    typesumperyear

---
+++ php/typesumperyear.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type: text/plain');
+
+$PDO = new PDO('sqlite:../data/bagofwords.db');
+$query = 'SELECT count(token) as summe, date FROM tokendatecount GROUP BY date ORDER BY date ASC';
+
+$tab = "\t";
+$nl = "\n";
+$res = '';
+
+foreach($PDO->query($query.';') as $row){
+	$res.=$row['date'].$tab.$row['summe'].$nl;
+}
+print($res);
+
+?>
