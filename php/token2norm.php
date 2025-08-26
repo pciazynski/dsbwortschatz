5aac646bc1ccc7e44d0f13416b70f6e969aeb697
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-08-26 12:28:50 +0200

    ..

--- php/token2norm.php
+++ php/token2norm.php
@@ -4,7 +4,7 @@ header('Content-Type: text/plain');
 if (isset($_GET['token'])){
 
 	$PDO = new PDO('sqlite:../data/normmapping.db');
-	$query = 'SELECT DISTINCT lemma,token,norm,type,subtype FROM tokennormtypesubtypedatefrequency';
+	$query = 'SELECT DISTINCT token,norm,type,subtype FROM tokennormtypesubtypedatefrequency';
 	$query .= ' WHERE token = "'.$_GET['token'].'"';
 
 	$tab = "\t";
@@ -12,7 +12,7 @@ if (isset($_GET['token'])){
 	$res = '';
 
 	foreach($PDO->query($query.';') as $row){
-		$res.=$row['lemma'].$tab.$row['token'].$tab.$row['norm'].$tab.$row['type'].$tab.$row['subtype'].$nl;
+		$res.=$tab.$row['token'].$tab.$row['norm'].$tab.$row['type'].$tab.$row['subtype'].$nl;
 	}
 	
 	print($res);
