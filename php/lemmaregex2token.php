57089fc59705222236594da99acdd9e343967de1
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-09-16 14:27:12 +0200

    bf lemma2regex

--- php/lemmaregex2token.php
+++ php/lemmaregex2token.php
@@ -8,15 +8,14 @@ if (isset($_GET['lemma'])){
 		return $hit;
 	}
 	$PDO->sqliteCreateFunction('regexp', '_sqliteRegexp', 2);
-	$query = 'SELECT lemma, token, SUM(frequency) as sumfreq FROM tokenlemmatypesubtypedatefrequency WHERE lemma REGEXP "\|'.$_GET['lemma'].'\|"  LIMIT 2100000';
+	$query = 'SELECT lemma, token, SUM(frequency) as sumfreq FROM tokenlemmatypesubtypedatefrequency WHERE lemma REGEXP "\|'.$_GET['lemma'].'\|"';
 	(isset($_GET['year'])) ? $query .= ' AND date '.$_GET['year'] : NULL;
 	$query.=' GROUP BY token,lemma';
 	(isset($_GET['sort'])) ? $query .= ' ORDER BY sumfreq DESC' : NULL;
 	$tab = "\t";
 	$nl = "\n";
 	$res = '';
-
-	foreach($PDO->query($query.";") as $row){
+	foreach($PDO->query($query."  LIMIT 2100000;") as $row){
 		$res.=$row['lemma'].$tab.$row['token'].$tab.$row['sumfreq'].$nl;
 	}
 	print($res);
