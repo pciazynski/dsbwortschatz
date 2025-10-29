9d1fee3406cdb45b092e590896edb9e5dc5760de
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-10-29 11:51:55 +0100

    .

--- _mjenjoweda.py
+++ _mjenjoweda.py
@@ -3,10 +3,11 @@ import shutil
 
 word_lemma = {}
 lemma_wordbag = {}
-items = {}
-lemmaitems = {}
+coords = {}
+lemmacoords = {}
 
 counter = 0
+#Funktioniert nicht wegen Orten, die aus mehreren Token bestehen wie Adamowa Kjarcma und Alt-Zauche
 
 def reset():
     if not os.path.exists("data"):
@@ -16,33 +17,42 @@ def reset():
     os.mkdir("data/mjenjoweda")
 reset()
 
+print("Reading coord")
+#lese ortsliste mit koordinaten als coords
 with open ("learn/ortsliste.txt","r",encoding="utf8") as oin:
     for line in oin:
         linearr = line.split("\t")
         tmp = linearr[0].lower()
-        items[tmp] = line
-            
+        coords[tmp] = linearr[1].replace("PLACE_","").strip()
+
+print("finding relevant lemma")
+#wenn token in coords, erzeuge lemmawordbag für lemma und uebertrage tokencoord in lemmacoord
 with open ("data/lemmamapping/_all.txt", "r", encoding="utf8") as bwin:
     for line in bwin:
         linearr = line.split("\t")
-        if linearr[0] in items:
+        if linearr[0] in coords:
             lemma_wordbag[linearr[1]] = {}
-            lemmaitems[linearr[1]] = items[linearr[0]]
-            
+            lemmacoords[linearr[1]] = coords[linearr[0]]
+
+print("mapping lemma to token")
+#für jedes lemma in lemmawordbag...
 with open ("data/lemmamapping/_all.txt", "r", encoding="utf8") as bwin:
     for line in bwin:
         linearr = line.split("\t")
         if linearr[1] in lemma_wordbag:
+#, wenn ein wort nicht damit verknüpft ist, verknüpfe es
             if not (linearr[0]) in lemma_wordbag[linearr[1]]:
                 tmp = lemma_wordbag[linearr[1]]
                 tmp[linearr[0]] = 1
                 lemma_wordbag[linearr[1]] = tmp
 
-with open("data/mjenjoweda/ortsliste.txt", "w", encoding="utf8") as out,open("data/mjenjoweda/ortsinfo.txt", "w", encoding="utf8") as out2:
+print("writing")
+with open("data/mjenjoweda/ortsliste.txt", "w", encoding="utf8") as ortslisteout,open("data/mjenjoweda/ortsinfo.txt", "w", encoding="utf8") as ortsinfoout:
     for lemma in sorted(lemma_wordbag):
         tmp = "|"
         for word in lemma_wordbag[lemma]:
             tmp+=word+"|"
-        out.write(lemma+"\t"+tmp+"\n")
-        out2.write(lemmaitems[lemma])
+        ortslisteout.write(lemma+"\t"+tmp+"\n")
+        ortsinfoout.write(lemma+"\t"+lemmacoords[lemma]+"\n")
     
+print("Done")
