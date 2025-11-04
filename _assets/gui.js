044e01cc019363138042c7fc9aadee749f875a2a
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-11-04 10:17:25 +0100

    project description

--- _assets/gui.js
+++ _assets/gui.js
@@ -1,3 +1,15 @@
+function getQueryVariable(variable) {
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=");
+		if (pair[0] == variable) {
+			return decodeURIComponent(pair[1]);
+		}
+	} 
+	return ""
+}
+
 function loading(elementid){
 	var ifrm = document.getElementById(elementid);
 	ifrm = ifrm.contentWindow || ifrm.contentDocument.document || ifrm.contentDocument;
