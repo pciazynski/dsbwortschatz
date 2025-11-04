497b0d730aa7c21d620588a04c5b5e4443c77cf5
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-11-04 15:49:59 +0100

    ..

--- php/psedcytassearch.php
+++ php/psedcytassearch.php
@@ -1,13 +1,13 @@
 <?php
 header('Content-Type: text/plain');
 
-if (isset($_GET['token'])){
-	$token = $_GET['token'];
-	$token = str_replace(array("ł","ć","č","ė","é","ě","ź","ž","ś","š","ŕ","ó"),array("l4","c1","c2","e1","e1","e2","z1","z2","s1","s2","r1","o1"),$token);
-	$token = str_replace(',','-%" OR url LIKE "',$token);
+if (isset($_GET['word'])){
+	$word = $_GET['word'];
+	$word = str_replace(array("ł","ć","č","ė","é","ě","ź","ž","ś","š","ŕ","ó"),array("l4","c1","c2","e1","e1","e2","z1","z2","s1","s2","r1","o1"),$word);
+	$word = str_replace(',','-%" OR url LIKE "',$word);
 
 	$PDO = new PDO('sqlite:../data/psedcytas.db');
-	$query = 'SELECT url FROM urls WHERE url LIKE "'.$token.'-%" LIMIT 1';
+	$query = 'SELECT url FROM urls WHERE url LIKE "'.$word.'-%" LIMIT 1';
 	
 	foreach($PDO->query($query.';') as $row){
 		print($row['url']);
