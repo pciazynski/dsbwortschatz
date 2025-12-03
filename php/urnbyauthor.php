5404d15a886b71cd4fd00f8d42cea78601b8aabb
JTiepmar <jochen.tiepmar@serbski-institut.de>
2025-12-03 09:00:02 +0100

    ..

---
+++ php/urnbyauthor.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: text/plain');
+
+if (isset($_GET['author'])){
+	$PDO = new PDO('sqlite:../data/authors.db');
+	$query = 'SELECT * FROM authors WHERE author ="'.$_GET['author'].'"';
+	(isset($_GET['year'])) ? $query .= ' AND year '.$_GET['year'] : NULL;
+	$tab = "\t";
+	$nl = "\n";
+	$res = '';
+
+	foreach($PDO->query($query.';') as $row){
+		$res.=$row['urn'].$tab.$row['title'].$tab.$row['year'].$tab.$row['coauthors'].$nl;
+	}
+	print($res);
+}
+
+?>
